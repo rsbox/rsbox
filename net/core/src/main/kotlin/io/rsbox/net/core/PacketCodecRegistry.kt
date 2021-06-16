81208a7654435706f9a48285d09607dc320d9e98
Kyle Escobar <82987787+kyleescobar@users.noreply.github.com>
2021-06-16 02:08:09 -0400

    Add support for packet data type and lengths within packet protocols

--- net/core/src/main/kotlin/io/rsbox/net/core/PacketCodecRegistry.kt
+++ net/core/src/main/kotlin/io/rsbox/net/core/PacketCodecRegistry.kt
@@ -6,6 +6,7 @@ class PacketCodecRegistry<out P : Packet> {
 
     private val opcodeTypeMap = hashMapOf<KClass<out P>, Int>()
     private val codecs = hashMapOf<Int, PacketCodec<P>>()
+    private val packetInfoMap = hashMapOf<Int, Pair<PacketDataType, Int>>()
 
     operator fun get(opcode: Int): PacketCodec<@UnsafeVariance P> = requireNotNull(this.codecs[opcode]) {
         "Unknown packet opcode: $opcode"
@@ -17,14 +18,27 @@ class PacketCodecRegistry<out P : Packet> {
         "Unknown packet type: ${type.simpleName}"
     }
 
+    fun getDataType(type: KClass<@UnsafeVariance P>): PacketDataType = requireNotNull(this.packetInfoMap[getOpcode(type)]?.first) {
+        "Unknown packet type: ${type.simpleName}"
+    }
+
+    fun getLength(type: KClass<@UnsafeVariance P>): Int = requireNotNull(this.packetInfoMap[getOpcode(type)]?.second) {
+        "Unknown packet type: ${type.simpleName}"
+    }
+
     @Suppress("UNCHECKED_CAST")
-    operator fun <T : @UnsafeVariance P> set(type: KClass<T>, opcode: Int, codec: PacketCodec<T>) {
+    operator fun <T : @UnsafeVariance P> set(type: KClass<T>, opcode: Int, dataType: PacketDataType, length: Int, codec: PacketCodec<T>) {
         this.opcodeTypeMap[type] = opcode
         this.codecs[opcode] = codec as PacketCodec<P>
+        this.packetInfoMap[opcode] = dataType to length
+    }
+
+    inline operator fun <reified T : @UnsafeVariance P> set(opcode: Int, length: Int, codec: PacketCodec<T>) {
+        this[T::class, opcode, PacketDataType.FIXED, length] = codec
     }
 
     inline operator fun <reified T : @UnsafeVariance P> set(opcode: Int, codec: PacketCodec<T>) {
-        this[T::class, opcode] = codec
+        this[T::class, opcode, PacketDataType.FIXED, -255] = codec
     }
 
     operator fun contains(opcode: Int): Boolean = opcode in this.codecs
---
+++ net/core/src/main/kotlin/io/rsbox/net/core/PacketDataType.kt
@@ -0,0 +1,9 @@
+package io.rsbox.net.core
+
+enum class PacketDataType {
+    FIXED,
+
+    VARIABLE_BYTE,
+
+    VARIABLE_SHORT
+}
